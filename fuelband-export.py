dd41fa2f41f75f6162bb5973f82678c760406145
burnedsap <salil458@gmail.com>
2016-03-27 01:09:17 +0530

    Exports Data to a CSV file
    I've modified the original code to store data in a CSV file. The CSV file can then be used for various other uses such as data visualisation.

--- fuelband-export.py
+++ fuelband-export.py
@@ -4,8 +4,9 @@ import json
 import urllib
 import urllib2
 import time
+import csv
 
-ACCESS_TOKEN = 'ACCESS CODE FROM NIKE'
+ACCESS_TOKEN = 'ACCESS CODE'
  
 base_url = 'https://api.nike.com'
 url = '/me/sport/activities/FUELBAND?access_token=%s&experienceType=FUELBAND' % ACCESS_TOKEN
@@ -14,7 +15,7 @@ current_month = None
 emotion = ''
 note = ''
 
-file = open('fuelband1.txt','w+')
+outputFile = open('fuelband.csv', 'w')
 
 while url:
  
@@ -35,26 +36,23 @@ while url:
 			if month != current_month:
 				current_month = month
 				#print ''
-				file.write('\n')
+				#file.write('\n')
+				outputWriter = csv.writer(outputFile)
+				outputWriter.writerow([current_month,'distance', 'fuel', 'calories', 'steps', 'duration', 'emotion', 'note'])
 				#print '--', current_month, '--'
-				file.write('--' + current_month + '--\n')
+				#file.write('--' + current_month + '--\n')
+				#file.write('\n')
 			
 			# distance calculation
 			metrics = activity.get('metricSummary')
 			kilom = metrics.get('distance')
 			distance = '%.2f' % round(kilom, 2)
 			
+			if kilom == 0:
+				kilom = 1
+			
 			# remove milliseconds
 			duration = metrics.get('duration').partition('.')[0]
-			
-			pace = ''
-			sp = duration.split(':')
-			if (len(sp) == 3):
-				duration_seconds = int(sp[0]) * 60 * 60 + int(sp[1]) * 60 + int(sp[2])
-				seconds_per_kilom = duration_seconds / kilom
-				hours, remainder = divmod(seconds_per_kilom, 3600)
-				minutes, seconds = divmod(remainder, 60)
-				pace = '(%.0f\'%02.0f/km)' % (minutes, seconds)
 
 			calories = metrics.get('calories')
 			fuel = metrics.get('fuel')
@@ -66,14 +64,15 @@ while url:
 				if (tag.get('tagType') == "NOTE") and (tag.get('tagValue') != "NOTE"):
 					note = tag.get('tagValue')
 
-			if activity.get('activityType') == "ALL_DAY": 
-				file.write(date + ' : distance: ' + str(distance).ljust(5) + 'km' + '\tpace: ' + str(pace).ljust(10) + '\tfuel: ' + str(fuel).ljust(5) + '\tcalories: ' +  str(calories).ljust(5) + '\tactive: '  + duration.ljust(5) + '\t' + emotion.ljust(5) + '\t' + note + ' ' + '\n')
-				emotion = '' 
-				note = ''
+
+			if activity.get('activityType') == "ALL_DAY":
+                                outputWriter.writerow([date, distance, fuel, calories, steps, duration, emotion, note])
+                                emotion = ''
+                                note = ''
 				
 	# pagination
 	url = None
 	if resp.get('paging') and resp.get('paging').get('next'):
 		url = resp.get('paging').get('next') 
 
-file.close()
+outputFile.close()
